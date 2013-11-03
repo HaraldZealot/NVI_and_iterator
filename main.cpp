c96cc3afb487d3e4e7db13b377edab6a14b0fcef
Harald Zealot <harald_zealot@tut.by>
2013-11-03 09:03:48 +0300

    try catch, change to simpletree

--- main.cpp
+++ main.cpp
@@ -5,14 +5,22 @@ using namespace std;
 
 int main()
 {
-    int array[7] = {4, 2, 6, 1, 3, 5, 7};
-    SimpleTree tree(array, 7);//  насамрэч я думаў тут кшталту палімарфізма Tree *tree = new SimpleTree(array,7);
-    //  але так таксама працуе :)
+    try
+    {
+        int array[7] = {4, 2, 6, 1, 3, 5, 7};
+        SimpleTree tree(array, 7);//  насамрэч я думаў тут кшталту палімарфізма Tree *tree = new SimpleTree(array,7);
+        //  але так таксама працуе :)
 
-    for(Tree::Iterator it = tree.begin(), end = tree.end(); it != end; ++it)
-        cout << *it << " ";
+        for(SimpleTree::Iterator it = tree.begin(), end = tree.end(); it != end; ++it)
+            cout << *it << " ";
 
-    cout << endl;
+        cout << endl;
+    }
+    catch(exception &e)
+    {
+        cerr << e.what() << endl;
+    }
+
     return 0;
 }
 
